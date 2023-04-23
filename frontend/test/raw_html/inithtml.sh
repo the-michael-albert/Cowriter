33c732ffd92caf38b07d1df7afef90b7c11991ae
MikeAl <michaeltesto15@hotmail.com>
2023-04-23 15:39:43 -0700

    updated files and added scripz

--- frontend/test/raw_html/inithtml.sh
+++ frontend/test/raw_html/inithtml.sh
@@ -1,22 +1,35 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "Please provide a parameter as a string"
+  exit 1
+fi
+
+dir_name="$1"
+
+cat > "${dir_name}.html" << EOF
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <!-- https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP -->
     <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline';">
-    <link href='treeview_style.css' rel='stylesheet'>
+    <link href='${dir_name}_style.css' rel='stylesheet'>
     <style>
     body {
         font-family: OpenSans, Arial, Helvetica, sans-serif;
     }
     </style>
-    <title>Treeview</title>
+    <title>${dir_name^}</title>
 </head>
 <body>
     <div id="container">
     </div>
-    <script src="treeview_elems.js"></script>
-    <script src="treeview_render.js"></script>
-    <script src="treeview_listen.js"></script>
+    <script src="${dir_name}_elems.js"></script>
+    <script src="${dir_name}_render.js"></script>
+    <script src="${dir_name}_listen.js"></script>
 </body>
 </html>
+EOF
+
+echo "HTML file created successfully!"
